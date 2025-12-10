0ee1e0d63e78b8f9b4cf301740c14a05e934c926
Kinetzki <kinetzki2k20@gmail.com>
2025-12-11 04:51:39 +0800

    fix: removed unused setup

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
-from typing import Union, List, Annotated
-from pydantic import BaseModel
-from fastapi import FastAPI, Depends, status
-from app.connection.database import engine,sessionLocal, Base
-from fastapi.middleware.cors import CORSMiddleware 
-from passlib.context import CryptContext
+from fastapi import FastAPI
+from app.connection.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 from app.connection.database import Base, engine
-from fastapi.openapi.utils import get_openapi
 from contextlib import asynccontextmanager
 from app.routes import router
 
@@ -18,35 +14,11 @@ async def lifespan(_):
 
 app = FastAPI(lifespan=lifespan)
 
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="Aqqire API",
-        version="1.0.0",
-        description="Property management API",
-        routes=app.routes,
-    )
-    openapi_schema["components"]["securitySchemes"] = {
-        "BearerAuth": {
-            "type": "http",
-            "scheme": "bearer",
-            "bearerFormat": "JWT",
-        }
-    }
-    openapi_schema["security"] = [{"BearerAuth": []}]
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-app.openapi = custom_openapi
-
 @app.middleware("http")
 async def log_headers(request, call_next):
     print("AUTH HEADER RECEIVED:", request.headers.get("authorization"))
     response = await call_next(request)
     return response
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 allow_origins = [
     "http://localhost:3000",
